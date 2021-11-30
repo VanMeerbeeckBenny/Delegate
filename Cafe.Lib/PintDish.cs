4fa535ce8c3c87be2ff0f27403057d971234f390
Benny <benny.van.meerbeeck@student.howest.be>
2021-11-30 11:37:29 +0100

    Add delegate

--- Cafe.Lib/PintDish.cs
+++ Cafe.Lib/PintDish.cs
@@ -4,6 +4,8 @@ namespace Cafe.Lib
 {
     public class PintDish
     {
+        public delegate void PinstartedHandler(object sender,EventArgs e);
+        public event PinstartedHandler PinStarted;
         public int PintCount { get; private set; }
         public int MaxPints { get; }
 
@@ -15,6 +17,7 @@ namespace Cafe.Lib
         public void AddPint()
         {
             if (PintCount >= MaxPints) throw new Exception("Dish full, no more pints for you!");
+            PinStarted?.Invoke(this, EventArgs.Empty);
             PintCount++;
         }
     }
